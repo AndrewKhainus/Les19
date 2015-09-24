94ce3b11b20cc17f1b053110d95480cfec4401e3
Andrew Khaynus <a.khaynus@gmail.com>
2015-09-24 12:20:43 +0300

    add getToken

--- app/src/main/java/com/radomar/les19/MainActivity.java
+++ app/src/main/java/com/radomar/les19/MainActivity.java
@@ -37,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initToolbar();
+        getToken();
 
         mDbHelper = new DatabaseHelper(this);
 
